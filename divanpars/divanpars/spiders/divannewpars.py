64318e2e2cf6b8ff4b5a3454dca179a559e93237
obogrelova <on.obogrelova@gmail.com>
2024-12-02 22:30:41 +0500

    new file

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        pass
