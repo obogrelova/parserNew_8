6d42ffcf936c6494789133841e3b3e15e914205f
obogrelova <on.obogrelova@gmail.com>
2024-12-03 21:41:38 +0500

    Update divannewpars.py

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -7,4 +7,10 @@ class DivannewparsSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        svets = response.css('div._Ud0k')
+        for svet in svets:
+            yield {
+                'name' : svet.css('div.lsooF span::text').get(),
+                'price' : svet.css('div.pY3d2 span::text').get(),
+                'url' : svet.css('a').attrib['href']
+            }
